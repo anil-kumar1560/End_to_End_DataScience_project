8e97d076926efd973523d8e255a3f99b73b2f55e
anil-kumar1560 <anilgummadidala20@gmail.com>
2025-05-09 18:31:26 -0400

    data Transformation

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 from src.DataScience import logger
+from src.DataScience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.DataScience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.DataScience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
-logger.info("Welcome to our custom logging Data Science")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
